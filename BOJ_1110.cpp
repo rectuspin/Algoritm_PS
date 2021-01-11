e78cfbb7d63c65dd94754b7790b78c716604e758
rectuspin <chrisyu94@gmail.com>
2021-01-11 22:02:54 +0900

    boj 1110

--- BOJ_1110.cpp
+++ BOJ_1110.cpp
@@ -4,16 +4,19 @@ using namespace std;
 
 int main()
 {
-	int i, n, x, t;
+	int n, x,y,z,cnt;
 
-	cin >> n >> x;
-	
-	for ( i = 0; i < n; i++){
-		cin >> t;
-		if (t < x)
-			cout << t<<" ";
-	}
+	cin >> n;
+	cnt = 0;
+	z = n;
+	do {
+		cnt++;
+		x = z;
+		y = (x / 10) + (x % 10);
+		z = (x % 10) * 10 + (y % 10);
+	} while (z != n);
 
+	cout << cnt;
 
 	return 0;
 
